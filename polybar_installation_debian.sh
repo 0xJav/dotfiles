06de0bd1b10b52a3cefd35703b43ae8a581c6768
Zpring <whuzz@protonmail.com>
2020-06-22 09:49:20 -0300

    polybar sccript updated

--- polybar_installation_debian.sh
+++ polybar_installation_debian.sh
@@ -6,7 +6,27 @@
 # .sh file made by Spring#8641
 
 # Dependencies required 
-sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
+sudo apt-get install -y \
+    cmake \
+    cmake-data \
+    libcairo2-dev \
+    libxcb1-dev \
+    libxcb-ewmh-dev \
+    libxcb-icccm4-dev \
+    libxcb-image0-dev \
+    libxcb-randr0-dev \
+    libxcb-util0-dev \
+    libxcb-xkb-dev \
+    pkg-config \
+    python-xcbgen \
+    xcb-proto \
+    libxcb-xrm-dev \
+    i3-wm \
+    libasound2-dev \
+    libmpdclient-dev \
+    libiw-dev \
+    libcurl4-openssl-dev \
+    libpulse-dev
 
 # Source for polybar
 git clone https://github.com/jaagr/polybar.git
