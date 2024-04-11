a3b0912757b528106a540a627020da9d438cbe16
mdxv <mohan.elias@proton.me>
2024-04-11 17:02:03 +0000

    add: new nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@ vim.call('plug#begin')
 Plug 'neovim/nvim-lspconfig'
 Plug 'sheerun/vim-polyglot'
 Plug 'nvim-lualine/lualine.nvim'
-Plug 'datsfilipe/vesper.nvim'
+Plug 'ellisonleao/gruvbox.nvim'
 Plug 'windwp/nvim-autopairs'
 vim.call('plug#end')
 
@@ -27,21 +27,34 @@ vim.cmd([[
   set mouse=
   set termguicolors
   set cursorline
-  colorscheme vesper
 ]])
 
-require('vesper').setup({
-    transparent = false, -- Boolean: Sets the background to transparent
-    italics = {
-        comments = true, -- Boolean: Italicizes comments
-        keywords = true, -- Boolean: Italicizes keywords
-        functions = true, -- Boolean: Italicizes functions
-        strings = true, -- Boolean: Italicizes strings
-        variables = true, -- Boolean: Italicizes variables
-    },
-    overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
-    palette_overrides = {}
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
 })
+vim.cmd("colorscheme gruvbox")
 
 require('lualine').setup()
 
